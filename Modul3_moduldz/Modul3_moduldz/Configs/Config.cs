7e855a17a22aa14cad83299764216ac8976291bc
denys-u <denys.udovik@nure.ua>
2021-04-04 22:40:17 +0300

    created and descrobed Config and Logger Config

---
+++ Modul3_moduldz/Modul3_moduldz/Configs/Config.cs
@@ -0,0 +1,11 @@
+﻿namespace Modul3_moduldz.Configs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Config
+    {
+        public LoggerConfig Logger { get; set; }
+    }
+}
---
+++ Modul3_moduldz/Modul3_moduldz/Configs/LoggerConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Modul3_moduldz.Configs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class LoggerConfig
+    {
+        public string DirectoryPath { get; set; }
+        public string ExtensionFile { get; set; }
+        public string NameFormat { get; set; }
+        public int BackUpCount { get; set; }
+    }
+}
