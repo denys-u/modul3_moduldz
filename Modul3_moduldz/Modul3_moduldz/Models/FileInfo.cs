fa435c5dcafe9c1a39aa267fbdb828319d4ae91e
denys-u <denys.udovik@nure.ua>
2021-04-05 23:50:15 +0300

    created FileInfo

---
+++ Modul3_moduldz/Modul3_moduldz/Models/FileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Modul3_moduldz.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class FileInfo
+    {
+        public string Path { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+    }
+}
